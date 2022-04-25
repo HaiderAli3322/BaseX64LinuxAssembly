1ae6eb485c15c62a20cd9df76a810ae29fbeef9a
Haider Ali <haider.ali2@ontariotechu.net>
2022-04-24 22:36:52 -0400

    first commit

--- main.asm
+++ main.asm
@@ -1,21 +1,17 @@
 extern printf
-extern exit
 
 global main
 
-section .text
+section .txt
 
 main:
-  ; printf("Hello, world!\n");
-  mov rdi, message
-  mov rax, 0
-  push rbx
-  call printf
-  pop rbx
+	mov rdi, greeting
+	mov rax, 0
+	mov rbx, 0
+	push rbx
+	call printf
+ 	pop rbx
 
-  ; exit(0)
-  mov rax, 0
-  call exit
-
+	ret
 section .data
-  message db "Hello, world!", 0ah, 0dh, 0
+	greeting db "Hello World" , 0ah , 0dh, 0
